0aa193d51d49c8de7357c07483944516aedb451e
gitcuaquan <79095757+gitcuaquan@users.noreply.github.com>
2024-06-21 18:04:07 +0700

    a

--- pages/don-hang-chua-tai-xe.vue
+++ pages/don-hang-chua-tai-xe.vue
@@ -3,13 +3,21 @@
    <div class="overflow-y-auto table-responsive">
       <UTable :loading="loading" :columns="columns" :rows="listOrder?.data">
          <template #short_id-data="{ row }">
-            <UBadge color="blue" variant="subtle">{{ row?.short_id }}</UBadge>
+            <UBadge color="sky" variant="subtle">{{ row?.short_id }}</UBadge>
          </template>
          <template #departure-data="{ row }">
-            {{ Object.values(row?.departure)?.join(" - ") }}
-         </template>
-         <template #destination-data="{ row }">
-            {{ Object.values(row?.destination)?.join(" - ") }}
+            <div class="flex flex-col">
+               <div class="flex items-center gap-2">
+                 <UIcon name="i-wpf-geo-fence" class="text-green-500" /> {{ Object.values(row?.departure)?.join(" - ") }}
+               </div>
+               <div class="flex items-center gap-2">
+                  <UIcon name="i-wpf-geo-fence" class="text-red-500" /> {{ Object.values(row?.destination)?.join(" - ") }}
+               </div>
+               <div class="flex items-center font-semibold gap-2" v-if="row?.note">
+                  <UIcon name="i-hugeicons-note-03" class="text-indigo-400" />  "{{row?.note}}"
+               </div>
+              
+            </div>
          </template>
          <template #date_of_destination-data="{ row }">
             {{ convertUTCToLocal(row?.date_of_destination) }}
@@ -17,8 +25,18 @@
          <template #price_guest-data="{ row }">
             {{ VND(row?.price_guest) }}
          </template>
-         <template #price-data="{ row }">
-            {{ VND(row?.price) }}
+         <template #price_sys-data="{ row }">
+            <div class="flex flex-col gap-2">
+               <div class="flex gap-2">
+                  <UBadge color="sky" class="w-20" variant="subtle">Tài xế thu</UBadge>   
+                  {{ VND(row?.price_guest) }}
+               </div>
+               <div class="flex gap-2">
+                  <UBadge color="green" class="w-20" variant="subtle">Tài xế nhận</UBadge>   
+                  {{ VND(row?.price) }}
+               </div>
+            </div>
+            
          </template>
          <template #action-data="{ row }">
             <UButton variant="soft">Nhận chuyến</UButton>
@@ -26,7 +44,7 @@
       </UTable>
    </div>
    <div class="flex">
-      <UPagination :page-count="10" v-model="parameters.page" class="mt-5 ms-auto me-10"
+      <UPagination :disabled="loading" :page-count="10" v-model="parameters.page" class="mt-5 ms-auto me-10"
          :total="listOrder?.pagination?.count ?? 1" />
    </div>
 </template>
@@ -48,25 +66,14 @@ const columns = [{
    label: 'Dịch vụ'
 }, {
    key: 'departure',
-   label: 'Điểm đón'
-}, {
-   key: 'destination',
-   label: 'Điểm trả'
+   label: 'Điểm đón - Điểm trả - Ghi chú'
 }, {
    key: 'date_of_destination',
    label: "Ngày đi"
 },
 {
-   key: 'price_guest',
-   label: "Cước thu"
-},
-{
-   key: 'price',
-   label: "Tài xế nhận"
-},
-{
-   key: 'note',
-   label: "Ghi chú"
+   key: 'price_sys',
+   label: "Giao dịch"
 },
 {
    key: 'action',
@@ -89,8 +96,6 @@ async function ListOrder(parameters: Partial<Pagination>, bodyFilter: Partial<Bo
 </script>
 
 <style>
-
-
 @media (min-width: 768px) {
    .table-responsive {
       width: calc(100vw - 290px);
